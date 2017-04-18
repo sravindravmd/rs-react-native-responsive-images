c1c126fc823bfe0854a758cb6b9841f2e808e16b
ravindras <sravindravmd@gmail.com>
2017-04-18 22:59:58 +0530

    repository initial stage

---
+++ react-native-responsive-image.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { Image } from 'react-native';
+import applyScale from './applyScale';
+export default class ResponsiveImage extends Component {
+
+    setNativeProps(nativeProps) {
+        this._root.setNativeProps(nativeProps);
+    }
+    
+    render(){
+        var width = applyScale(this.props.initWidth);
+        var height = applyScale(this.props.initHeight);
+        return (
+            <Image style={[{width: width, height: height}, this.props.style]}
+                   source={this.props.source}
+                   ref={component => this._root = component}
+                   resizeMode={this.props.resize ? 'contain' : 'stretch'}
+                   onLoadStart={this.props.onLoadStart}
+                   onProgress={this.props.onProgress}
+                   onLoad={this.props.onLoad}
+                   onError={this.props.onError}
+                   onLoadEnd={this.props.onLoadEnd}
+                   >
+                {this.props.children}
+            </Image>
+        );
+    }
+}
+
